3ecb95134ba314a5fe4861b77ebe246b9a6a8db7
Evgeniya Kudinova <evgenyakott2728@gmail.com>
2022-09-13 20:07:13 +0400

    First commit

---
+++ src/API/Fetch.js
@@ -0,0 +1,36 @@
+const UrlCompetitions = "https://api.football-data.org/v2/competitions/"
+const UrlTeams = "https://api.football-data.org/v2/teams/"
+
+export const getLeaguesData = async () => {
+    /*прочитываем url адрес и получаем данные*/
+  const api = await fetch(UrlCompetitions, {
+    headers: {
+      "x-auth-token": process.env.REACT_APP_API_KEY
+    }
+  })
+  /*если запрос прошел*/
+  if (api.ok) {
+    /*переводим данные в json формат*/
+    const json = await api.json()
+    return json
+  }
+  else {
+    console.error("Возникла проблема с получением данных о лигах")
+    return null
+  }
+}
+
+export const getTeams = async () => {
+  const api = await fetch(UrlTeams, {
+    headers: {
+      "x-auth-token": process.env.REACT_APP_API_KEY
+    }
+  })
+  if (api.ok) {
+    const json = await api.json()
+    return json
+  } else {
+    console.error("Возникла проблема с получением данных о командах")
+    return null
+  }
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+
+/*хуко состояние для вывода "0" */
+/*создаются 2 переменные. 2-ая перем. использ.для обно"---------");*/
+const [value, setValue] = useState(0);
+
+function Navbar() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
